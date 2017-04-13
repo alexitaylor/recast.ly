8cb86cbd99c3470d7f13a594e8516b324980762b
Hack Reactor Student <student@hackreactor.com>
2017-04-12 19:50:20 -0700

    Should detect typing in search bar, does not submit search yet

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,41 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+class Search extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: ''
+		}
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleEventChange = this.handleEventChange.bind(this);
+	}	
+
+	handleEventChange(event) {
+		console.log(event.target.value);
+		this.setState({
+			value: event.target.value
+		})
+	}
+
+	handleSubmit(event) {
+		console.log(this.state.value);
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<div className="search-bar form-inline">
+    		<input className="form-control" type="text" value={this.state.value} onChange={this.handleEventChange} />
+    		<button className="btn hidden-sm-down" onClick={this.handleSubmit}>
+      		<span className="glyphicon glyphicon-search"></span>
+    		</button>
+  		</div> 
+		);
+	}
+}
+
+// var Search = () => (
+  
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://www.googleapis.com/youtube/v3/search/' + options,
+  type: 'GET',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+  	callback(data);
+    console.log('Success');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('Failed', data);
+  }
+});
+
+
 };
 
 window.searchYouTube = searchYouTube;
